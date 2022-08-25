f99a17363ab44df02d76c1b4731582712333f248
Gustavo Amaral <guga.adr@gmail.com>
2022-08-24 21:48:04 -0300

    Kernel não funcional, mas quem sabe...

--- TESTE_1.C
+++ TESTE_1.C
@@ -1,15 +1,67 @@
+/* Implementation of some processes, with simple printf function,
+for testing the Kernel Scheduler */
+
 #include <kernel.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void far iniciaProcess() {
-    int i;
-    for (i = 0; i < 100; i++) {
-        printf ("Testando");
+#define MAX_TIME 10000
+
+void far process1() {
+    int i = 0;
+    while (i < MAX_TIME) {
+        printf("1\n");
         i++;
     }
+
+    /* Terminando o Processo...*/
+    terminaProcesso();
+}
+
+void far process2() {
+    int i = 0;
+    while (i < MAX_TIME) {
+        printf("2\n");
+        i++;
+    }
+
+    /* Terminando o Processo...*/
+    terminaProcesso();
+}
+
+void far process3() {
+    int i = 0;
+    while (i < MAX_TIME) {
+        printf("3\n");
+        i++;
+    }
+
+    /* Terminando o Processo...*/
+    terminaProcesso();
+}
+
+void far process4() {
+    int i = 0;
+    while (i < MAX_TIME) {
+        printf("4\n");
+        i++;
+    }
+
+    /* Terminando o Processo...*/
     terminaProcesso();
 }
 
 void main() {
-    
+    /* Iniciando a fila de processos */
+    iniciaFilaProc();
+
+    /* Criacao dos Processos */
+    criaProcessos(process1, "P1");
+    criaProcessos(process2, "P2");
+    criaProcessos(process3, "P3");
+    criaProcessos(process4, "P4");
+
+    /* Transferindo o controle para o Escalonador */
+    ativaEscalador();
+    system("pause");
 }
